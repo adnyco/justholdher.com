dbc1bd06b69a4a851235d2733fd4da9f40cc4027
Adny <48308530+adnyco@users.noreply.github.com>
2026-02-22 20:17:45 -0800

    Update substack.js

--- substack.js
+++ substack.js
@@ -11,7 +11,6 @@
   function formatDate(dateString) {
     const date = new Date(dateString);
     return date.toLocaleDateString(undefined, {
-      weekday: "long",
       year: "numeric",
       month: "long",
       day: "numeric"
